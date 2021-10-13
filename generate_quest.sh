05b05fc0dcd5cdb13d1cd7415d15c311b00695c9
Jesse Mu <6245103+jayelm@users.noreply.github.com>
2021-10-12 20:24:50 -0700

    reduce #

--- generate_quest.sh
+++ generate_quest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python -m minihack.scripts.play --env MiniHack-Quest-Easy-v0 --mode random --no-render --ngames 100000
+python -m minihack.scripts.play --env MiniHack-Quest-Easy-v0 --mode random --no-render --ngames 10000
--- generate_room.sh
+++ generate_room.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python -m minihack.scripts.play --env MiniHack-Room-5x5-v0 --mode random --no-render --ngames 100000
+python -m minihack.scripts.play --env MiniHack-Room-5x5-v0 --mode random --no-render --ngames 10000
